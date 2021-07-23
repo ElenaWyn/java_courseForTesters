e65ace51110e256d0bf4f65ad65d0212f0d3e1aa
Elena <elena.slobodian@gmail.com>
2021-07-23 13:01:36 +0200

    Общая инфраструктура (шаблон) для тестового набора (Mantis)

---
+++ mantis-tests/src/test/java/mantis/tests/TestBase.java
@@ -0,0 +1,28 @@
+package mantis.tests;
+
+import mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.stream.Collectors;
+
+public class TestBase {
+
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.logout();
+        app.stop();
+    }
+
+
+
+
+}
