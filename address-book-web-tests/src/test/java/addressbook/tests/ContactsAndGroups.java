68776001d701823d5b7700cf9dc9f7212696a357
Elena <elena.slobodian@gmail.com>
2021-07-26 21:08:11 +0200

    Исправленное задание 16

--- address-book-web-tests/src/test/java/addressbook/tests/ContactsAndGroups.java
+++ address-book-web-tests/src/test/java/addressbook/tests/ContactsAndGroups.java
@@ -12,16 +12,20 @@ import org.testng.annotations.Test;
 public class ContactsAndGroups  extends TestBase{
     @BeforeMethod
     public void ensurePreconditions() {
+        //if there is no groups
         if(app.db().groups().size() == 0) {
             app.goTo().GroupPage();
             app.group().create(new Group().withGroupName("Grupa").withGroupHeader("TestGroupHeader").withGroupFooter("TestGroupFooter"));
 
         }
+        //if there is no contacts
         if (app.db().contacts().size() == 0) {
             app.contact().create(new Contact().withFirstname("Elena").withLastname("Doe").withAddress("Uliczna 5").withTel_home("1234567"));
         }
+
         ContactSet contacts = app.db().contacts();
         GroupSet groups = app.db().groups();
+        //is all contacts in all groups?
         int contactsInAllGroups = 0;
         int isInAllGroups = 0;
         for (Contact contact : contacts) {
@@ -35,6 +39,7 @@ public class ContactsAndGroups  extends TestBase{
             }
             contactsInAllGroups = 0;
         }
+        //if all contacts is in all groups create new contact
         if (contactsInAllGroups == contacts.size()) {
             app.contact().create(new Contact().withFirstname("Olena").withLastname("Doedoe").withAddress("Uliczna 5").withTel_home("1234567"));
         }
@@ -45,14 +50,26 @@ public class ContactsAndGroups  extends TestBase{
     public void addContactToGroup(){
         ContactSet contacts = app.db().contacts();
         GroupSet groups = app.db().groups();
-        Group groupToAdd = app.contact().randomGroup(groups); // define group for adding
+
         Contact modifiedContact = app.contact().randomContact(contacts); //define contact which will be added to selected group
 
         //assert if contact is already in selected group or contact is in all groups. If yes, choose a new contact
-        if (app.contact().isContactInGroup(modifiedContact, groupToAdd) || modifiedContact.getGroups().size() == groups.size()){
+        while (modifiedContact.getGroups().size() == groups.size()){
             modifiedContact = app.contact().randomContact(contacts);
         }
         GroupSet contactGroups = modifiedContact.getGroups();
+
+        //choose group to add contact in
+        for (Group group : groups) {
+            for (Group group1 : contactGroups) {
+                if (group1 == group) {
+                    groups.without(group1);
+                }
+            }
+        }
+        Group groupToAdd = app.contact().randomGroup(groups); // define group for adding
+
+
         app.contact().addToGroup(modifiedContact, groupToAdd);
         contactGroups = contactGroups.withAdded(groupToAdd);
 
